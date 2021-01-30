fd7e0a0ed54e50c2896d9c4432a07399b937a11a
Minimi <bambee18341@gmail.com>
2021-01-31 02:12:31 +0700

    personal tax

--- app/src/main/java/com/example/mytaxplanner/MainActivity.kt
+++ app/src/main/java/com/example/mytaxplanner/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.mytaxplanner.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -13,36 +17,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.fragment_home -> {
-                    // Respond to navigation item 1 click
-                    Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.fragment_income -> {
-                    // Respond to navigation item 2 click
-                    Toast.makeText(this,"Income",Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.fragment_file -> {
-                    // Respond to navigation item 2 click
-                    Toast.makeText(this,"File",Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.fragment_tax -> {
-                    // Respond to navigation item 2 click
-                    Toast.makeText(this,"Tax",Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.fragment_suggest -> {
-                    // Respond to navigation item 2 click
-                    Toast.makeText(this,"Suggest",Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> false
-            }
-        }
+//        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+//            when(item.itemId) {
+//                R.id.fragment_home -> {
+//                    // Respond to navigation item 1 click
+//                    Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                R.id.fragment_income -> {
+//                    // Respond to navigation item 2 click
+//                    Toast.makeText(this,"Income",Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                R.id.fragment_file -> {
+//                    // Respond to navigation item 2 click
+//                    Toast.makeText(this,"File",Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                R.id.fragment_tax -> {
+//                    // Respond to navigation item 2 click
+//                    Toast.makeText(this,"Tax",Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                R.id.fragment_suggest -> {
+//                    // Respond to navigation item 2 click
+//                    Toast.makeText(this,"Suggest",Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+        val navigation = findNavController(R.id.fragment)
+        val config = AppBarConfiguration(setOf(R.id.homeFragment,R.id.incomeFragment,R.id.fileFragment,R.id.taxFragment,R.id.suggestFragment))
+        binding.bottomNavigation.setupWithNavController(navigation)
+        setupActionBarWithNavController(navigation,config)
     }
 
 }
--- app/src/main/java/com/example/mytaxplanner/fragment/TaxFragment.kt
+++ app/src/main/java/com/example/mytaxplanner/fragment/TaxFragment.kt
@@ -1,10 +1,11 @@
-package com.example.mytaxplanner
+package com.example.mytaxplanner.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.mytaxplanner.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FileFragment.newInstance] factory method to
+ * Use the [TaxFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FileFragment : Fragment() {
+class TaxFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class FileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_file, container, false)
+        return inflater.inflate(R.layout.fragment_tax, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class FileFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FileFragment.
+         * @return A new instance of fragment TaxFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FileFragment().apply {
+            TaxFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
